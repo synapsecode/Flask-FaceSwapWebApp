e4de95c9f835bf2d0e041cedb9babc28b33a986e
Manas Hejmadi <manas2all@outlook.com>
2022-01-11 22:53:29 +0530

    JSShorten

--- static/app.js
+++ static/app.js
@@ -7,32 +7,22 @@ output_face_display = document.getElementById("oface");
 merge_btn = document.getElementById("mergebtn");
 downloadbtn = document.getElementById("o_down");
 
-API_URL = "http://localhost:5000";
+updateFace = (evt, disp) => {
+	var tgt = evt.target,
+    files = tgt.files;
+	var fr = new FileReader();
+	fr.onload = () => {disp.src = fr.result};
+	fr.readAsDataURL(files[0]);
+}
 
 //load Main Face
 mf_upload_btn.onchange = (evt) => {
 	console.log("Updating Main Face");
-	// Display new picture
-	var tgt = evt.target || window.event.srcElement,
-    files = tgt.files;
-
-	var fr = new FileReader();
-	fr.onload = function () {
-		main_face_display.src = fr.result;
-	}
-	fr.readAsDataURL(files[0]);
+	updateFace(evt, main_face_display)
 }
 
 //load Second Face
 sf_upload_btn.onchange = (evt) => {
 	console.log("Updating Second Face");
-	// Display new picture
-	var tgt = evt.target || window.event.srcElement,
-    files = tgt.files;
-
-	var fr = new FileReader();
-	fr.onload = function () {
-		second_face_display.src = fr.result;
-	}
-	fr.readAsDataURL(files[0]);
+	updateFace(evt, second_face_display)
 }
